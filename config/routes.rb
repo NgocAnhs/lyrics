3459b451959a35ed8ba5e8def43ef1d6265e726b
anhtn98 <anhtn@zigexn.vn>
2025-03-11 09:47:52 +0700

    feat: create seed song

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   root 'songs#index'
-  resources :songs, only: :index
+  resources :songs, only: %i[index show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+songs_master = YAML.load_file('db/master/songs.yml')
+if Song.count.zero?
+  songs_master.each do |song_master|
+    song = Song.find_or_initialize_by(name: song_master['name'])
+    song.lyrics = File.read(Rails.root.join(song_master['lyrics_filename']))
+    song.save
+  end
+end
